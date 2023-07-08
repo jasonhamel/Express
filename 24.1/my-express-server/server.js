eef2894d8fe46b8191651cbe27a94e3bea357735
Jason <github@jasongetsemail.com>
2023-07-07 20:03:34 -0400

    Added a home route. Logged the request and returned an h1 as the response

--- 24.1/my-express-server/server.js
+++ 24.1/my-express-server/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 const app = express();
 
+app.get("/", function (req, res) {
+  console.log(req);
+  res.send("<h1>Hello from Express!</h1>");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
