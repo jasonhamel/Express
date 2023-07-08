9036b6a8fa96fc0dbb4b47797b9ca4c3a72105eb
Jason <github@jasongetsemail.com>
2023-07-08 12:45:38 -0400

    Learned about the sendFile() method. Also googled to fix an error with __dirname as the video is outdated

--- 24.2/calculator_challenge/calculator.js
+++ 24.2/calculator_challenge/calculator.js
@@ -1,8 +1,14 @@
 import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function () {
