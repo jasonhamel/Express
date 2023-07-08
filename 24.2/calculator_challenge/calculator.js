216fb915c2e173384f85acfd5af2771b8ede22aa
Jason <github@jasongetsemail.com>
2023-07-08 17:03:31 -0400

    Created another page for a BMI Calculator

--- 24.2/calculator_challenge/calculator.js
+++ 24.2/calculator_challenge/calculator.js
@@ -15,9 +15,28 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
 app.post("/", function (req, res) {
   let result = Number(req.body.num1) + Number(req.body.num2);
-  res.send("<p>The sum of the two numbers submitted is: </p>" + result);
+  res.send(
+    "<h1>Very Basic Calculator</h1>" +
+      "<p>The sum of the two numbers submitted is: </p>" +
+      result
+  );
+});
+
+app.post("/bmicalculator", function (req, res) {
+  let height = parseFloat(req.body.height) * parseFloat(req.body.height);
+  let weight = parseFloat(req.body.weight);
+  let result = Math.round((weight / height) * 10) / 10;
+  res.send(
+    "<h1>BMI Calculator</h1>" +
+      "<p>The BMI for the entered height and weight is: </p>" +
+      result
+  );
 });
 
 app.listen(3000, function () {
