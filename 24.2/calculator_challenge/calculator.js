48dda9aafd1e67bddefcda25be8bb17d3bbd2f2d
Jason <github@jasongetsemail.com>
2023-07-08 13:10:10 -0400

    Completed the post request to return the sum. Used body-parser

--- 24.2/calculator_challenge/calculator.js
+++ 24.2/calculator_challenge/calculator.js
@@ -2,13 +2,22 @@ import express from "express";
 import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyparser from "body-parser";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const bodyParser = bodyparser;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  let result = Number(req.body.num1) + Number(req.body.num2);
+  res.send("<p>The sum of the two numbers submitted is: </p>" + result);
 });
 
 app.listen(3000, function () {
