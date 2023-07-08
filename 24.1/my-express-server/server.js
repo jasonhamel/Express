6547ca8626dd3d8368cce829ac277a8c860de17e
Jason <github@jasongetsemail.com>
2023-07-07 20:18:00 -0400

    Added routes for contact and about.

--- 24.1/my-express-server/server.js
+++ 24.1/my-express-server/server.js
@@ -6,6 +6,20 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello from Express!</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send(
+    "Please conact me at <a href=https://threads.net/sirculling> Threads </a>"
+  );
+});
+
+app.get("/about", function (req, res) {
+  res.send(
+    "Hi! My name is Jason. I'm a career nerd that's trying" +
+      " to learn enough about full stack development to not" +
+      " embarass myself when applying for jobs."
+  );
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
